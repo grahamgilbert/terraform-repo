a16344a7ef07b26d2022747d1835aeb1eee642bc
Graham Gilbert <graham.gilbert@airbnb.com>
2018-10-31 19:58:25 -0700

    Edge function for index.html redirect

---
+++ lambda.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "iam_for_redirect_lambda" {
+  name = "iam_for_redirect_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+          "edgelambda.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "archive_file" "redirect_lambda_zip" {
+  type = "zip"
+
+  output_path             = "redirect_lambda.zip"
+  source_file = "${path.module}/lambda_code/redirect.js"
+}
+
+resource "aws_lambda_function" "redirect_lambda" {
+  filename         = "redirect_lambda.zip"
+  function_name    = "redirect_lambda"
+  role             = "${aws_iam_role.iam_for_redirect_lambda.arn}"
+  handler          = "redirect.handler"
+  source_code_hash = "${data.archive_file.redirect_lambda_zip.output_base64sha256}"
+  runtime          = "nodejs8.10"
+  publish          = true
+}
---
+++ lambda_code/redirect.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Redirects URLs to default document. Examples:
+ *
+ * /blog            -> /blog/index.html
+ * /blog/july/      -> /blog/july/index.html
+ * /blog/header.png -> /blog/header.png
+ *
+ */
+
+let defaultDocument = 'index.html';
+
+exports.handler = (event, context, callback) => {
+    const request = event.Records[0].cf.request;
+
+    if(request.uri != "/") {
+        let paths = request.uri.split('/');
+        let lastPath = paths[paths.length - 1];
+        let isFile = lastPath.split('.').length > 1;
+
+        if(!isFile) {
+            if(lastPath != "") {
+                request.uri += "/";
+            }
+
+            request.uri += defaultDocument;
+        }
+
+        console.log(request.uri);
+    }
+
+    callback(null, request);
+};
