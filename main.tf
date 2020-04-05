4e533160f6a3c2c14941f12ecbc3e2e9ed3d85c8
Graham Gilbert <graham.gilbert@airbnb.com>
2020-04-05 13:32:44 -0700

    FINE

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_acm_certificate" "certificate" {
 
 module "dns" {
   source                         = "./dns"
-  main_cloudfront_name           = "${aws_cloudfront_distribution.www_distribution.domain_name}"
-  main_cloudfront_hosted_zone_id = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
+  # main_cloudfront_name           = "${aws_cloudfront_distribution.www_distribution.domain_name}"
+  # main_cloudfront_hosted_zone_id = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
 }
