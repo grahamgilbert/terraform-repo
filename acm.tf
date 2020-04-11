09237d2ea12594c010b83972f99f789c74163430
Graham Gilbert <graham.gilbert@airbnb.com>
2020-04-11 13:47:36 -0700

    Fix the certificate

---
+++ acm.tf
@@ -0,0 +1,23 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.${var.root_domain_name}"
+  validation_method = "DNS"
+  subject_alternative_names = ["${var.root_domain_name}"]
+}
+
+data "aws_route53_zone" "zone" {
+  name         = "${var.root_domain_name}."
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.zone.id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "aws_acm_certificate" "certificate" {
-  domain_name       = "*.${var.root_domain_name}"
-  validation_method = "EMAIL"
-
-  subject_alternative_names = ["${var.root_domain_name}"]
-}
-
 module "dns" {
   source                         = "./dns"
   main_cloudfront_name           = "${aws_cloudfront_distribution.www_distribution.domain_name}"
