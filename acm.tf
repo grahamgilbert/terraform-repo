44b24812842ca5a331038240ec01affc5bb0b61d
Graham Gilbert <graham.gilbert@airbnb.com>
2020-12-27 16:51:46 -0800

    fmt

--- acm.tf
+++ acm.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "*.${var.root_domain_name}"
-  validation_method = "DNS"
+  domain_name               = "*.${var.root_domain_name}"
+  validation_method         = "DNS"
   subject_alternative_names = ["${var.root_domain_name}"]
 }
 
