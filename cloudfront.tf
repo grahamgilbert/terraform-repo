f47f42ef40ce5c6cd4073bb73eaaad211732cff4
Graham Gilbert <graham.gilbert@airbnb.com>
2020-12-29 22:05:57 -0800

    set

--- cloudfront.tf
+++ cloudfront.tf
@@ -17,10 +17,6 @@ resource "aws_cloudfront_distribution" "www_distribution" {
 
   // All values are defaults from the AWS console.
   default_cache_behavior {
-    lambda_function_association {
-      event_type = "origin-request"
-      lambda_arn = "${aws_lambda_function.redirect_lambda.arn}:${aws_lambda_function.redirect_lambda.version}"
-    }
 
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
