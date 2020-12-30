1b028d57110a20a4a84b11c9730f83d21ba216a8
Graham Gilbert <graham.gilbert@airbnb.com>
2020-12-29 22:03:44 -0800

    iam user

--- s3.tf
+++ s3.tf
@@ -80,3 +80,24 @@ resource "aws_s3_bucket" "log_bucket" {
     }
   }
 }
+
+s3_actions   = ["s3:GetObject"]
+s3_resources = "arn:aws:s3:::examplebucket/*"
+
+data "aws_iam_policy_document" "ggcom_policydoc" {
+  statement {
+    actions   = ["s3:*"]
+    resources = "${aws_s3_bucket.www.arn}/*"
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_user" "www_deploy_user" {
+  name = "www_deploy_user"
+}
+
+resource "aws_iam_user_policy" "www_deploy_user_policy" {
+  name   = aws_iam_user_policy.name
+  user   = aws_iam_user_policy.name
+  policy = join("", data.aws_iam_policy_document.ggcom_policydoc.*.json)
+}
