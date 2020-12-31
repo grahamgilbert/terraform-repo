0d4255ec5b8ab9e6b2306d31afbfa9a47a6fcb9f
Graham Gilbert <graham.gilbert@airbnb.com>
2020-12-30 20:09:28 -0800

    editor

--- salversion.tf
+++ salversion.tf
@@ -24,29 +24,35 @@ resource "google_service_account" "deploy_account" {
   project      = google_project.salversion_project.project_id
 }
 
-resource "google_service_account_iam_member" "deployer" {
+resource "google_service_account_iam_member" "editor" {
   service_account_id = google_service_account.deploy_account.name
-  role               = "roles/appengine.appAdmin"
+  role               = "roles/editor"
   member             = "serviceAccount:${google_service_account.deploy_account.email}"
 }
 
-resource "google_service_account_iam_member" "service_account" {
-  service_account_id = google_service_account.deploy_account.name
-  role               = "roles/iam.serviceAccountUser"
-  member             = "serviceAccount:${google_service_account.deploy_account.email}"
-}
+# resource "google_service_account_iam_member" "deployer" {
+#   service_account_id = google_service_account.deploy_account.name
+#   role               = "roles/appengine.appAdmin"
+#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# }
 
-resource "google_service_account_iam_member" "service_admin" {
-  service_account_id = google_service_account.deploy_account.name
-  role               = "roles/appengine.serviceAdmin"
-  member             = "serviceAccount:${google_service_account.deploy_account.email}"
-}
+# resource "google_service_account_iam_member" "service_account" {
+#   service_account_id = google_service_account.deploy_account.name
+#   role               = "roles/iam.serviceAccountUser"
+#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# }
 
-resource "google_service_account_iam_member" "build_editor" {
-  service_account_id = google_service_account.deploy_account.name
-  role               = "roles/cloudbuild.builds.editor"
-  member             = "serviceAccount:${google_service_account.deploy_account.email}"
-}
+# resource "google_service_account_iam_member" "service_admin" {
+#   service_account_id = google_service_account.deploy_account.name
+#   role               = "roles/appengine.serviceAdmin"
+#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# }
+
+# resource "google_service_account_iam_member" "build_editor" {
+#   service_account_id = google_service_account.deploy_account.name
+#   role               = "roles/cloudbuild.builds.editor"
+#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# }
 
 resource "google_app_engine_application" "salversion" {
   project       = google_project.salversion_project.project_id
