c365518021b09817c787cb5e0b8b2a3f2d2e33ed
Graham Gilbert <graham.gilbert@airbnb.com>
2022-11-15 14:45:41 -0800

    bye

--- salversion.tf
+++ salversion.tf
@@ -1,76 +1,76 @@
-resource "random_id" "salversion_id" {
-  byte_length = "2"
-  prefix      = "salversion-"
-}
+# resource "random_id" "salversion_id" {
+#   byte_length = "2"
+#   prefix      = "salversion-"
+# }
 
-resource "google_project" "salversion_project" {
-  name            = "salversion"
-  project_id      = random_id.salversion_id.hex
-  billing_account = var.billing_account_id
-  org_id          = var.org_id
-  labels          = var.labels
-}
+# resource "google_project" "salversion_project" {
+#   name            = "salversion"
+#   project_id      = random_id.salversion_id.hex
+#   billing_account = var.billing_account_id
+#   org_id          = var.org_id
+#   labels          = var.labels
+# }
 
-resource "google_project_service" "salversion_services" {
-  for_each           = toset(var.services)
-  service            = each.key
-  project            = google_project.salversion_project.project_id
-  disable_on_destroy = false
-}
+# resource "google_project_service" "salversion_services" {
+#   for_each           = toset(var.services)
+#   service            = each.key
+#   project            = google_project.salversion_project.project_id
+#   disable_on_destroy = false
+# }
 
-resource "google_service_account" "deploy_account" {
-  account_id   = "deploy-account"
-  display_name = "Deploy Account"
-  project      = google_project.salversion_project.project_id
-}
+# resource "google_service_account" "deploy_account" {
+#   account_id   = "deploy-account"
+#   display_name = "Deploy Account"
+#   project      = google_project.salversion_project.project_id
+# }
 
-resource "google_service_account_iam_member" "editor" {
-  service_account_id = google_service_account.deploy_account.name
-  role               = "roles/editor"
-  member             = "serviceAccount:${google_service_account.deploy_account.email}"
-}
-
-resource "google_service_account_iam_member" "owner" {
-  service_account_id = google_service_account.deploy_account.name
-  role               = "roles/owner"
-  member             = "serviceAccount:${google_service_account.deploy_account.email}"
-}
-
-resource "google_app_engine_domain_mapping" "domain_mapping" {
-  domain_name = "version.salopensource.com"
-
-  ssl_settings {
-    ssl_management_type = "AUTOMATIC"
-  }
-  project = google_project.salversion_project.project_id
-}
-
-# resource "google_service_account_iam_member" "deployer" {
+# resource "google_service_account_iam_member" "editor" {
 #   service_account_id = google_service_account.deploy_account.name
-#   role               = "roles/appengine.appAdmin"
+#   role               = "roles/editor"
 #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
 # }
 
-# resource "google_service_account_iam_member" "service_account" {
+# resource "google_service_account_iam_member" "owner" {
 #   service_account_id = google_service_account.deploy_account.name
-#   role               = "roles/iam.serviceAccountUser"
+#   role               = "roles/owner"
 #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
 # }
 
-# resource "google_service_account_iam_member" "service_admin" {
-#   service_account_id = google_service_account.deploy_account.name
-#   role               = "roles/appengine.serviceAdmin"
-#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# resource "google_app_engine_domain_mapping" "domain_mapping" {
+#   domain_name = "version.salopensource.com"
+
+#   ssl_settings {
+#     ssl_management_type = "AUTOMATIC"
+#   }
+#   project = google_project.salversion_project.project_id
 # }
 
-# resource "google_service_account_iam_member" "build_editor" {
-#   service_account_id = google_service_account.deploy_account.name
-#   role               = "roles/cloudbuild.builds.editor"
-#   member             = "serviceAccount:${google_service_account.deploy_account.email}"
-# }
+# # resource "google_service_account_iam_member" "deployer" {
+# #   service_account_id = google_service_account.deploy_account.name
+# #   role               = "roles/appengine.appAdmin"
+# #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# # }
 
-resource "google_app_engine_application" "salversion" {
-  project       = google_project.salversion_project.project_id
-  location_id   = "us-central"
-  database_type = "CLOUD_FIRESTORE"
-}
+# # resource "google_service_account_iam_member" "service_account" {
+# #   service_account_id = google_service_account.deploy_account.name
+# #   role               = "roles/iam.serviceAccountUser"
+# #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# # }
+
+# # resource "google_service_account_iam_member" "service_admin" {
+# #   service_account_id = google_service_account.deploy_account.name
+# #   role               = "roles/appengine.serviceAdmin"
+# #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# # }
+
+# # resource "google_service_account_iam_member" "build_editor" {
+# #   service_account_id = google_service_account.deploy_account.name
+# #   role               = "roles/cloudbuild.builds.editor"
+# #   member             = "serviceAccount:${google_service_account.deploy_account.email}"
+# # }
+
+# resource "google_app_engine_application" "salversion" {
+#   project       = google_project.salversion_project.project_id
+#   location_id   = "us-central"
+#   database_type = "CLOUD_FIRESTORE"
+# }
